750861e1fbcb17f9870aca581e503c5eeefdaa98
Jakub Rzepinski <100721114+rzepa000@users.noreply.github.com>
2025-11-09 21:55:42 +0000

    dress

--- js/clock.js
+++ js/clock.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
   let currentDate = new Date();
 
   // Target future date/24 hour time/Timezone
-  let targetDate = moment.tz("2026-04-10 13:30", "Manila/Philippines");
+  let targetDate = moment.tz("2026-04-10 14:00", "Manila/Philippines");
 
   // Calculate the difference in seconds between the future and current date
   let diff = targetDate / 1000 - currentDate.getTime() / 1000;
